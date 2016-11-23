51a0e9f68ac4886fd9c0baff1b688907a1e2421b
haner27 <369685930@qq.com>
2016-11-23 18:11:43 +0800

    aa

--- test.py
+++
@@ -1,4 +0,0 @@
-import hashlib
-import random
-from datetime import datetime
-print '{0}{1}'.format(datetime.now().strftime('%Y%m%d%H%M%S'), str(random.randint(100, 999)))
--- update.sh
+++
@@ -1,2 +0,0 @@
-git pull
-uwsgi --reload rosehj.pid

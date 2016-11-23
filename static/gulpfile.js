25ab9b4a4a33aaac7838890b5078b348cece2367
haner27 <369685930@qq.com>
2016-11-23 18:12:35 +0800

    aa

--- static/gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require("gulp"),
-    minCss = require("gulp-minify-css");
-var concat = require('gulp-concat');
-gulp.task("minwebcss",function () {
-    return gulp.src(['css/common/*/*.css','css/admin/operate/*.css'])
-        .pipe(concat("all.min.css"))
-        .pipe(minCss())
-    .pipe(gulp.dest("css/compile/"))
-})
